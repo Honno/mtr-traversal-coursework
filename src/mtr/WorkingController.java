45e183cdbd0ccc9bc17a7778b13a7a9cc1849cea
Matthew Barber <barberm1@aston.ac.uk>
2017-12-04 14:42:51 +0000

    formated dom's comments and made them slightly more readable

--- src/mtr/WorkingController.java
+++ src/mtr/WorkingController.java
@@ -21,11 +21,14 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class WorkingController implements Controller {
-
+	// key: terminal line, value: array of terminals in respective line
 	private Map<String, String[]> lineMap;
+	// key: terminal name, value: respective Node object of terminal
 	private Map<String, Node<String, String>> nodesMap;
 
 	/**
+	 * Convert given CSV file to create the respective line map, and use that to create a nodes map.
+	 * 
 	 * @param path the path to the csv file
 	 * @throws FileNotFoundException in case of file not existing
 	 * @throws IOException in case of the file reading the file with insufficient permissions
@@ -45,17 +48,19 @@ public class WorkingController implements Controller {
 	 */
 	public void generateLineMap(String path)
 			throws FileNotFoundException, IOException {
+		// initialise map that stores terminal lines and respective terminals
 		lineMap = new HashMap<String, String[]>();
-		String line = "";
+		
+		//iterate through each line in the csv
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-
+			String line = "";
 			while ((line = br.readLine()) != null) {
-
-				String[] lineElements = line.split(",");								//split line information by commas
-				lineMap.put(lineElements[0], Arrays.copyOfRange(lineElements, 1,		//add the line name and associated terminals
+				// split line elements by commas
+				String[] lineElements = line.split(",");
+				// add the line name (first element) and associated terminals (subsequent elements)
+				lineMap.put(lineElements[0], Arrays.copyOfRange(lineElements, 1,		
 						lineElements.length));
 			}
-
 		} catch (FileNotFoundException fnfe) {
 			fnfe.printStackTrace();
 		} catch (IOException ioe) {
@@ -68,60 +73,88 @@ public class WorkingController implements Controller {
 	 * 
 	 */
 	public void generateNodesMap() {
+		// initialise map that stores all the terminal nodes, with the respective terminal's name as key
 		nodesMap = new HashMap<String, Node<String, String>>();
 
+		// iterate through every terminal line
 		for (Entry<String, String[]> pair : lineMap.entrySet()) {
+			// retrieve terminal line
 			String line = pair.getKey();
+			// retrieve terminals
 			String[] terminals = pair.getValue();
 
-			Iterator<String> itr = Arrays.asList(terminals).iterator();
-
+			// declare and initialise a variable to indicate if the current iteration is the first
 			boolean first = true;
 
+			// store the node found in the previous iteration to be used in creating an edge
 			Node<String, String> prevNode = null;
 
+			// iterate through the terminals
+			Iterator<String> itr = Arrays.asList(terminals).iterator();
 			while (itr.hasNext()) {
-				String terminal = itr.next();										//retrieve the next terminal
-				Node<String, String> node;											
-				if (nodesMap.containsKey(terminal)) {								//check if terminal already exists
-					node = nodesMap.get(terminal);									//retrieve the terminal
+				// retrieve the next terminal
+				String terminal = itr.next();
+				
+				// find already existing node of the terminal, or create a new one
+				Node<String, String> node;
+				if (nodesMap.containsKey(terminal)) {
+					node = nodesMap.get(terminal);									
 				} else {
-					node = new Node<String, String>(terminal);						//create a new terminal
+					node = new Node<String, String>(terminal);						
 				}
-				if (first) {														//check if it is the first iteration
-					first = false;													//allow connections to be made
+				
+				// check if it is the first iteration 
+				if (first) {
+					// ignore creating an edge between a (non-existent) previous node
+					// tell the subsequent iterations that the first iteration has occurred
+					first = false;
+				// create edge between current node and previous node
 				} else {
-					Edge<String, String> edge = new Edge<String, String>(			//create a new terminal link
+					// create a new edge between two nodes
+					Edge<String, String> edge = new Edge<String, String>(			
 							prevNode, node, line);
-					prevNode.addEdge(edge);											//add the terminal link to two terminals
+					// add the generated edge to the two terminals
+					prevNode.addEdge(edge);											
 					node.addEdge(edge);
 				}
-				nodesMap.put(terminal, node);										//add the terminal name and terminal to the map
-				prevNode = node;													//store the previously modified terminal
+				// add the terminal to the nodes map with the key as the terminal's name
+				nodesMap.put(terminal, node);
+				
+				// store the previously modified terminal
+				prevNode = node;													
 			}
 		}
 	}
 
 	@Override
 	public String listAllTermini() {
-		StringBuffer sb = new StringBuffer();			
-		Iterator<String> itr = nodesMap.keySet().iterator();						//iterate through the terminals
+		StringBuffer sb = new StringBuffer();
+		
+		// iterate through the terminals
+		Iterator<String> itr = nodesMap.keySet().iterator();						
 		while (itr.hasNext()) {											
-			sb.append(itr.next());													//append the next terminal
+			//append the next terminal to the string buffer
+			sb.append(itr.next());
+			
+			// if there is another terminal to add in the next iteration, add a comma separator
 			if (itr.hasNext()) {
 				sb.append(", ");
 			}
 		}
-		return sb.toString();														//return the terminal list
+		return sb.toString();														
 	}
 
 	
 	@Override
 	public String listStationsInLine(String line) {
 		try {
-			String[] terminals = lineMap.get(line);									//retrieve the target line
+			// retrieve the the terminals of the passed in terminal line
+			String[] terminals = lineMap.get(line);
+			
+			// check if terminal line exists for error handling
 			if (terminals != null) {
-				return String.join(", ", terminals);								//concatenate and return the terminals in the line
+				// if the terminal line exists, concatenate and return the terminals in the line
+				return String.join(", ", terminals);								
 			} else {
 				throw new NoSuchElementException("terminal line " + line
 						+ " does not exist");
