b4441bb4161518b04c934f5b62f386ffe17da41b
Matthew Barber <quitesimplymatt@gmail.com>
2017-12-10 23:06:20 +0000

    finished comments on listAllDirectlyConnectedLines, minor fixes elsewhere

--- src/mtr/WorkingController.java
+++ src/mtr/WorkingController.java
@@ -21,7 +21,8 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
- * An implementation of the Controller interface that meets the coursework requirements.
+ * An implementation of the Controller interface that meets the coursework
+ * requirements.
  */
 public class WorkingController implements Controller {
 	// line name: array of stations in respective line
@@ -30,11 +31,16 @@ public class WorkingController implements Controller {
 	private Map<String, Node<String, String>> nodesMap;
 
 	/**
-	 * Convert given CSV file to create the respective line map, and use the line map to create a nodes map.
+	 * Convert given CSV file to create the respective line map, and use the
+	 * line map to create a nodes map.
 	 * 
-	 * @param path the path to the csv file
-	 * @throws FileNotFoundException in case of file not existing
-	 * @throws IOException in case of the file reading the file with insufficient permissions
+	 * @param path
+	 *            the path to the csv file
+	 * @throws FileNotFoundException
+	 *             in case of file not existing
+	 * @throws IOException
+	 *             in case of the file reading the file with insufficient
+	 *             permissions
 	 */
 	public WorkingController(String path) throws FileNotFoundException,
 			IOException {
@@ -43,26 +49,31 @@ public class WorkingController implements Controller {
 	}
 
 	/**
-	 * Generates a HashMap that represents the MTR line's CSV with the station line and respective stations.
+	 * Generates a HashMap that represents the MTR network with the station
+	 * lines and respective stations.
 	 * 
-	 * @param path path of the file containing the line information
-	 * @throws FileNotFoundException in case of file path not existing
-	 * @throws IOException in the case of reading the file with insufficient permissions
+	 * @param path
+	 *            path of the file containing the line information
+	 * @throws FileNotFoundException
+	 *             in case of file path not existing
+	 * @throws IOException
+	 *             in the case of reading the file with insufficient permissions
 	 */
-	public void generateLineMap(String path)
-			throws FileNotFoundException, IOException {
+	public void generateLineMap(String path) throws FileNotFoundException,
+			IOException {
 		// initialise map that stores station lines and respective stations
 		lineMap = new HashMap<String, String[]>();
-		
-		//iterate through each line in the csv
+
+		// iterate through each line in the csv
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = "";
 			while ((line = br.readLine()) != null) {
 				String[] lineElements = line.split(",");
-				
-				// add the line name (first element) and associated stations (subsequent elements)
-				lineMap.put(lineElements[0], Arrays.copyOfRange(lineElements, 1,		
-						lineElements.length));
+
+				// add the line name (first element) and associated stations
+				// (subsequent elements)
+				lineMap.put(lineElements[0], Arrays.copyOfRange(lineElements,
+						1, lineElements.length));
 			}
 		} catch (FileNotFoundException fnfe) {
 			fnfe.printStackTrace();
@@ -70,12 +81,14 @@ public class WorkingController implements Controller {
 			ioe.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Generates a HashMap that represents the MTR stations and their connections
+	 * Generates a HashMap that represents the MTR network with the stations and
+	 * all their immediate connections.
 	 */
 	public void generateNodesMap() {
-		// initialise map that stores all the station nodes, with the respective station's name as key
+		// initialise map that stores all the station nodes, with the respective
+		// station's name as key
 		nodesMap = new HashMap<String, Node<String, String>>();
 
 		// iterate through every station line
@@ -85,10 +98,12 @@ public class WorkingController implements Controller {
 			// retrieve stations
 			String[] stations = pair.getValue();
 
-			// declare and initialise a variable to indicate if the current iteration is the first
+			// declare and initialise a variable to indicate if the current
+			// iteration is the first
 			boolean first = true;
 
-			// store the node found in the previous iteration to be used in creating an edge
+			// store the node found in the previous iteration to be used in
+			// creating an edge
 			Node<String, String> prevNode = null;
 
 			// iterate through the stations
@@ -96,33 +111,37 @@ public class WorkingController implements Controller {
 			while (itr.hasNext()) {
 				// retrieve the next station
 				String station = itr.next();
-				
-				// retrieve already existing node of the station, or create a new one
+
+				// retrieve already existing node of the station, or create a
+				// new one
 				Node<String, String> node;
 				if (nodesMap.containsKey(station)) {
-					node = nodesMap.get(station);									
+					node = nodesMap.get(station);
 				} else {
-					node = new Node<String, String>(station);						
+					node = new Node<String, String>(station);
 				}
-				
-				// check if it is the first iteration 
+
+				// check if it is the first iteration
 				if (first) {
-					// ignore creating an edge between a (non-existent) previous node and tell the subsequent iterations that the first iteration has occurred
+					// ignore creating an edge between a (non-existent) previous
+					// node and tell the subsequent iterations that the first
+					// iteration has occurred
 					first = false;
-				// create edge between current node and previous node
+					// create edge between current node and previous node
 				} else {
 					// create a new edge between two nodes
-					Edge<String, String> edge = new Edge<String, String>(			
+					Edge<String, String> edge = new Edge<String, String>(
 							prevNode, node, line);
 					// add the generated edge to the two stations
-					prevNode.addEdge(edge);											
+					prevNode.addEdge(edge);
 					node.addEdge(edge);
 				}
-				// add the station to the nodes map with the key as the station's name
+				// add the station to the nodes map with the key as the
+				// station's name
 				nodesMap.put(station, node);
-				
+
 				// store the previously modified station
-				prevNode = node;													
+				prevNode = node;
 			}
 		}
 	}
@@ -130,37 +149,37 @@ public class WorkingController implements Controller {
 	@Override
 	public String listAllTermini() {
 		StringBuffer sb = new StringBuffer();
-		
+
 		// iterate through every station line
 		Iterator<Entry<String, String[]>> itr = lineMap.entrySet().iterator();
-		while(itr.hasNext()) {
-			Entry<String, String[]>  entry = itr.next();
-			
+		while (itr.hasNext()) {
+			Entry<String, String[]> entry = itr.next();
+
 			// add line to buffer
 			sb.append(entry.getKey());
 			sb.append(": ");
-			
+
 			// add line's respective stations to buffer
 			String[] stations = entry.getValue();
 			sb.append(stations[0]);
 			sb.append(" <-> ");
 			sb.append(stations[stations.length - 1]);
-			
-			if(itr.hasNext()) {
+
+			if (itr.hasNext()) {
 				sb.append("\n");
 			}
 		}
-		
+
 		return sb.toString();
 	}
 
-	
 	@Override
 	public String listStationsInLine(String line) {
 		try {
 			String[] stations = lineMap.get(line);
-			
-			// if the station line exists, concatenate and return the stations in the line
+
+			// if the station line exists, concatenate and return the stations
+			// in the line
 			if (stations != null) {
 				return String.join(", ", stations);
 			} else {
@@ -172,26 +191,38 @@ public class WorkingController implements Controller {
 		}
 	}
 
-	
 	@Override
 	public String listAllDirectlyConnectedLines(String line) {
 		try {
-			String[] stations = lineMap.get(line);											
+			String[] stations = lineMap.get(line);
+
+			// if station line exists, list all the connected lines
 			if (stations != null) {
+				// find all station nodes in the line
 				List<Node<String, String>> nodes = new ArrayList<Node<String, String>>();
 				for (String station : stations) {
 					nodes.add(nodesMap.get(station));
 				}
+
+				// initialise store of all connected lines
 				Set<String> lines = new HashSet<String>();
+
+				// iterate through every node in the line
 				for (Node<String, String> node : nodes) {
+
+					// iterate through every edge of the line
 					Set<Edge<String, String>> edges = node.getEdges();
 					for (Edge<String, String> edge : edges) {
+						// store edge's weight (in this case the connection's
+						// line)
 						lines.add(edge.getWeight());
 					}
 				}
 
+				// remove given line from store as it's useless information
 				lines.remove(line);
 
+				// concatenate and return the connected lines
 				return String.join(", ", lines);
 			} else {
 				throw new NoSuchElementException("Station line " + line
@@ -201,8 +232,6 @@ public class WorkingController implements Controller {
 			return e.getMessage();
 		}
 	}
-
-	
 
 	@SuppressWarnings("finally")
 	@Override
@@ -216,8 +245,8 @@ public class WorkingController implements Controller {
 			boolean endIsNull = end == null;
 			if (startIsNull || endIsNull) {
 				if (startIsNull && endIsNull) {
-					throw new NoSuchElementException(
-							"Both " + stationA + " and " + stationB + " stations do not exist");
+					throw new NoSuchElementException("Both " + stationA
+							+ " and " + stationB + " stations do not exist");
 				} else if (startIsNull) {
 					throw new NoSuchElementException("Station " + stationA
 							+ " does not exist");
