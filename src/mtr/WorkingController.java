e7767f7ca9bf20500db9b29d2d45fa929dd3fed1
Barber <barberm1@aston.ac.uk>
2017-12-12 15:17:16 +0000

    add case for duplicate nodes on showPathBetween

--- src/mtr/WorkingController.java
+++ src/mtr/WorkingController.java
@@ -267,31 +267,37 @@ public class WorkingController implements Controller {
 				// find a path between start and end stations
 				List<Edge<String, String>> path = Node.bfs(start, end);
 
-				// store previous node of iteration, initialise with start node
-				Node<String, String> prevNode = start;
 				// start output with starting node
-				sb.append(start.toString() + " -> ");
-
-				// iterate through edges in path
-				Iterator<Edge<String, String>> itr = path.iterator();
-				while (itr.hasNext()) {
-					// store next edge of the path
-					Edge<String, String> edge = itr.next();
-					// retrieve the other node of the edge by passing the known
-					// previous node
-					Node<String, String> node = edge.getNode(prevNode);
-
-					// append connecting node to output
-					sb.append(node.getContent());
-
-					// if there are more edges to add, append a separator string
-					// to output
-					if (itr.hasNext()) {
-						sb.append(" -> ");
+				sb.append(start.toString());
+				
+				// check if path is empty (i.e. start station and end station is the same)
+				if(!path.isEmpty()) {
+					// store previous node of iteration, initialise with start node
+					Node<String, String> prevNode = start;
+					// append a separator string
+					sb.append( " -> ");
+	
+					// iterate through edges in path
+					Iterator<Edge<String, String>> itr = path.iterator();
+					while (itr.hasNext()) {
+						// store next edge of the path
+						Edge<String, String> edge = itr.next();
+						// retrieve the other node of the edge by passing the known
+						// previous node
+						Node<String, String> node = edge.getNode(prevNode);
+	
+						// append connecting node to output
+						sb.append(node.getContent());
+	
+						// if there are more edges to add, append a separator string
+						// to output
+						if (itr.hasNext()) {
+							sb.append(" -> ");
+						}
+	
+						// store current node as previous node
+						prevNode = node;
 					}
-
-					// store current node as previous node
-					prevNode = node;
 				}
 
 				output = sb.toString();
